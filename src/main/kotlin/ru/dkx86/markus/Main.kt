ce3f67bf7d141b1992c8bfca6c188451085ba33b
Dmitry Kuznetsov <6352927+dkx86@users.noreply.github.com>
2021-12-23 09:40:01 +0300

    Help message and license text

--- src/main/kotlin/ru/dkx86/markus/Main.kt
+++ src/main/kotlin/ru/dkx86/markus/Main.kt
@@ -33,6 +33,7 @@ fun processCommand(commandLine: String) {
     val arg = if (parts.size > 1) parts[1] else ""
     when (parts.first()) {
         "help" -> printHelp()
+        "license" -> printLicense()
         "create" -> createNewProject()
         "edit" -> validate(arg, ::editProject)
         "delete" -> validate(arg, ::deleteProject)
@@ -42,8 +43,6 @@ fun processCommand(commandLine: String) {
         "exit" -> exitProcess(0)
     }
 }
-
-fun printHelp() = println("Commands: list, create, show, edit, delete, help, convert, exit")
 
 fun validate(arg: String, operation: (index: Int) -> Unit) {
     try {
@@ -162,4 +161,42 @@ fun deleteProject(index: Int) {
         saveProjects()
         println("Project has been successfully DELETED.")
     }
+}
+
+fun printHelp() {
+    println("'MarKus-html v0.1' a simple tool from making static html-weblog from markdown text.\n")
+    println("Commands:\n")
+    println("list       : list existed projects")
+    println("show n     : show detailed information about project #n")
+    println("create     : create new project")
+    println("edit n     : edit project #n")
+    println("delete n   : delete project #n")
+    println("convert n  : convert project #n to html")
+    println("exit       : exit the program")
+    println("help       : print this help")
+    println("license    : print license text")
+}
+
+fun printLicense() {
+    println("MIT License\n" +
+            "\n" +
+            "Copyright (c) 2021 Dmitry Kuznetsov aka dkx86\n" +
+            "\n" +
+            "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+            "of this software and associated documentation files (the \"Software\"), to deal\n" +
+            "in the Software without restriction, including without limitation the rights\n" +
+            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+            "copies of the Software, and to permit persons to whom the Software is\n" +
+            "furnished to do so, subject to the following conditions:\n" +
+            "\n" +
+            "The above copyright notice and this permission notice shall be included in all\n" +
+            "copies or substantial portions of the Software.\n" +
+            "\n" +
+            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+            "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
+            "SOFTWARE.")
 }
